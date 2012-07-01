5aaf3bdcd12669d2827ee9310a7b6dac863d97f9
Teemu Rytkonen <teemu.rytkonen@nokia.com>
2012-07-01 13:55:51 +0300

    initial version of salibandy models and rest

--- torpedo/statistics/models.py
+++ torpedo/statistics/models.py
@@ -1,7 +1,69 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
 
-
-class MyModel(models.Model):
+class League(models.Model):
+    """
+    League is a set of games for a certain period of time.
+    """
     name =  models.TextField()
+    from_date = models.DateField()
+    to_date = models.DateField()
+
+
+class Team(models.Model):
+    """
+    Team is a set of players.
+    """
+    name =  models.TextField()
+
+    def __unicode__(self):
+        return "Team: %r" % self.name
+
+class Game(models.Model):
+    date = models.DateTimeField()
+    location = models.TextField()
+    home = models.ForeignKey(Team, related_name='home')
+    guest = models.ForeignKey(Team, related_name='guest')
+
+    def __unicode__(self):
+        return "Game: %s, %r - %r" % (self.date, self.home, self.guest)
+
+class Player(models.Model):
+    number = models.IntegerField()
+    name = models.TextField()
+    role= models.TextField(blank=True)
+    team = models.ForeignKey(Team, related_name='player')
+
+    def __unicode__(self):
+        return "Player: #%r: %r" % (self.number, self.name)
+
+class Goal(models.Model):
+    time = models.TimeField(null=False)
+    game = models.ForeignKey(Game)
+    team = models.ForeignKey(Team)
+    player = models.ForeignKey(Player, related_name='scoring')
+    assisting = models.ForeignKey(Player, related_name='assisting')
+    note = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return "Goal: %r, %r" % (self.player, self.time)
+
+class Penalty(models.Model):
+    time = models.TimeField(null=False)
+    length = models.TimeField(null=False) 
+    game = models.ForeignKey(Game)
+    team = models.ForeignKey(Team)
+    player = models.ForeignKey(Player)
+    reason = models.IntegerField()
+    note = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return "Penalty: %r,  %r" % (self.time, self.player)
+
+admin.site.register(League)
+admin.site.register(Team)
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Goal)
+admin.site.register(Penalty)
--- torpedo/statistics/urls.py
+++ torpedo/statistics/urls.py
@@ -4,16 +4,46 @@ Created on 28.6.2012
 @author: teerytko
 '''
 
-from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import patterns, url
 from djangorestframework.resources import ModelResource
 from djangorestframework.views import ListOrCreateModelView, InstanceModelView
-from statistics.models import MyModel
+from statistics.models import Team, League, Game, Player, Goal, Penalty
 
-class MyResource(ModelResource):
-    model = MyModel
+
+class TeamResource(ModelResource):
+    model = Team
+
+
+class LeagueResource(ModelResource):
+    model = League
+
+
+class GameResource(ModelResource):
+    model = Game
+
+
+class PlayerResource(ModelResource):
+    model = Player
+
+
+class GoalResource(ModelResource):
+    model = Goal
+
+
+class PenaltyResource(ModelResource):
+    model = Penalty
 
 urlpatterns = patterns('',
-    url(r'mymodel$', ListOrCreateModelView.as_view(resource=MyResource)),
-    url(r'mymodel(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=MyResource)),
+    url(r'rest/league$', ListOrCreateModelView.as_view(resource=LeagueResource)),
+    url(r'rest/league/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=LeagueResource)),
+    url(r'rest/team$', ListOrCreateModelView.as_view(resource=TeamResource)),
+    url(r'rest/team/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=TeamResource)),
+    url(r'rest/game$', ListOrCreateModelView.as_view(resource=GameResource)),
+    url(r'rest/game/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=GameResource)),
+    url(r'rest/player$', ListOrCreateModelView.as_view(resource=PlayerResource)),
+    url(r'rest/player/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=PlayerResource)),
+    url(r'rest/goal$', ListOrCreateModelView.as_view(resource=GoalResource)),
+    url(r'rest/goal/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=GoalResource)),
+    url(r'rest/penalty$', ListOrCreateModelView.as_view(resource=PenaltyResource)),
+    url(r'rest/penalty/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=PenaltyResource)),
 )
