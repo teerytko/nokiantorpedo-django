23577fba1f147b43d1b1bd3b336d65b18ddd2080
Teemu Rytkonen <teemu.rytkonen@nokia.com>
2012-08-17 13:53:55 +0300

    update logout link

--- torpedo/torpedo_main/urls.py
+++ torpedo/torpedo_main/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
     url(r'^$', 'torpedo_main.views.home', name='home'),
     url(r'^calendar$', 'torpedo_main.views.calendar', name='calendar'),
     url(r'^association$', 'torpedo_main.views.association', name='association'),
-    (r'^account/', include('django_authopenid.urls')),
+    url(r'^account/', include('django_authopenid.urls')),
+    url(r'^account/logout/$', 'django.contrib.auth.views.logout', 
+        {'next_page': '/'}),
 )
