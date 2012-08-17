68f834d7f9016a977f32d76c9f220add0f4a3c8e
Teemu Rytkonen <teemu.rytkonen@nokia.com>
2012-08-17 16:48:26 +0300

    refactored the templates to use {{STATIC_URL}}

--- torpedo/torpedo_main/urls.py
+++ torpedo/torpedo_main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     url(r'^calendar$', 'torpedo_main.views.calendar', name='calendar'),
     url(r'^association$', 'torpedo_main.views.association', name='association'),
     url(r'^account/', include('django_authopenid.urls')),
+    url(r'^forum/account/', include('django_authopenid.urls')),
     url(r'^account/logout/$', 'django.contrib.auth.views.logout', 
         {'next_page': '/'}),
 )
