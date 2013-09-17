398f8d8d3b8c0387309f424f755f808bbffb96df
tsrytkon <teemu.s.rytkonen@intel.com>
2013-09-17 19:19:00 +0300

    Test fb feeds

--- torpedo/torpedo_main/static/js/index.view.js
+++ torpedo/torpedo_main/static/js/index.view.js
@@ -3,7 +3,15 @@ require(['jquery', 'bootstrap', 'feeds'], function($, bootstrap, feeds) {
 	leftfeed = {'titleurl': '/forum/2/',
 				'feedurl': '/forum/feeds/posts/'};
 
+	var facebook = function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/fi_FI/all.js#xfbml=1&appId=205098582998927";
+		  fjs.parentNode.insertBefore(js, fjs);
+	};
 	$(document).ready(function() {
+		facebook(document, 'script', 'facebook-jssdk');
 	});
 });
 }).call(this);
